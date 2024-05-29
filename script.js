9385b88b8bff57318798094be475919301fc6777
Busiii <jennifer-li.beeler@stud.hslu.ch>
2024-05-29 10:46:28 +0200

    Uii more pictures to click

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function changeImage()
-{
-var image = document.getElementById('image');
-image.src = "pictures_green/gp2.png";
+let x = 0; 
+const images = ["pictures_green/gp2", "pictures_green/gp3", "pictures_green/gp4", "pictures_green/gp5", "pictures_green/gp6", "pictures_green/gp7", "pictures_green/gp8", "pictures_green/gp9", "pictures_green/gp10"];
+
+function changeImage() {
+    var image = document.getElementById('image');
+    image.src = images[x] + ".png";
+    x = (x + 1) % images.length; 
 }
