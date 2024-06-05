c8cf5fb73db39180e31b570ec50f0dd8d896bfbd
Busiii <jennifer-li.beeler@stud.hslu.ch>
2024-06-05 11:40:41 +0200

    fff

--- Tryout/script.js
+++ Tryout/script.js
@@ -5,11 +5,34 @@ const images = [
     "pictures_green/gp8.png", "pictures_green/gp9.png", "pictures_green/gp10.png"
 ];
 
+const positions = [
+    { left: '42%', top: '38%' },
+    { left: '10%', top: '10%' },
+    { left: '70%', top: '20%' },
+    { left: '20%', top: '70%' },
+    { left: '80%', top: '50%' },
+    { left: '30%', top: '80%' },
+    { left: '50%', top: '10%' },
+    { left: '10%', top: '50%' },
+    { left: '90%', top: '90%' }
+];
+
+
 function changeImage() {
     currentIndex = (currentIndex + 1) % images.length;
     document.getElementById('image').src = images[currentIndex];
+
+    const button = document.querySelector('button');
+    const newPosition = positions[currentIndex];
+    button.style.left = newPosition.left;
+    button.style.top = newPosition.top;
 }
 
 window.onload = function() {
     document.getElementById('image').src = images[0];
+
+    const button = document.querySelector('button');
+    const initialPosition = positions[0];
+    button.style.left = initialPosition.left;
+    button.style.top = initialPosition.top;
 };
