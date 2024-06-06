ba1115276599896f19db5a535935385adcc2bb31
Busiii <jennifer-li.beeler@stud.hslu.ch>
2024-06-06 17:32:54 +0200

    aa

--- lvl7.js
+++ lvl7.js
@@ -21,29 +21,17 @@ const sizes = [
 ];
 
 const divPositions = [
-    { left: '20%', top: '70%' },
+    { left: '72%', top: '21%' },
     { left: '28%', top: '53%' },
-    { left: '75%', top: '70%' },
-    { left: '32%', top: '59.5%' },
-    { left: '45.5%', top: '42%' },
-    { left: '54%', top: '45%' },
-    { left: '45.5%', top: '42%' },
-    { left: '54%', top: '45%' },
-    { left: '45.5%', top: '42%' }
-    // Add more positions as needed
+    { left: '0%', top: '21%' },
+    { left: '0%', top: '0%' }
 ];
 
 const divSizes = [
-    { width: '0px', height: '0px' },
+    { width: '120px', height: '30px' },
     { width: '250px', height: '250px' },
-    { width: '200px', height: '200px' },
-    { width: '40px', height: '40px' },
-    { width: '200px', height: '200px' },
-    { width: '200px', height: '200px' },
-    { width: '200px', height: '200px' },
-    { width: '200px', height: '200px' },
-    { width: '200px', height: '200px' },
-    // Add more sizes as needed
+    { width: '600px', height: '200px' },
+    { width: '0px', height: '0px' }
 ];
 
 const phrases = [
@@ -108,7 +96,7 @@ function changeImage() {
 
             // Check if it is the last image and redirect if true
             if (currentIndex === 0) { // This means it has looped back to the first image
-                window.location.href = 'levelZwei.html'; 
+                window.location.href = 'index.html'; 
             }
         }, 250); // 250 milliseconds for fade-out transition
 
