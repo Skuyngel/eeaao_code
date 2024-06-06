e4669c81c61a0f18ea9d1c0439c8962fad5316dd
Angelika-Gort <angelika.gort@stud.hslu.ch>
2024-06-06 15:35:18 +0200

    ye

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const sizes = [
 const divPositions = [
     { left: '20%', top: '70%' },
     { left: '28%', top: '53%' },
-    { left: '45.5%', top: '42%' },
+    { left: '75%', top: '70%' },
     { left: '54%', top: '45%' },
     { left: '45.5%', top: '42%' },
     { left: '54%', top: '45%' },
@@ -49,7 +49,7 @@ const divPositions = [
 ];
 
 const divSizes = [
-    { width: '200px', height: '200px' },
+    { width: '0px', height: '0px' },
     { width: '250px', height: '250px' },
     { width: '200px', height: '200px' },
     { width: '200px', height: '200px' },
