02119d92add6ca3240b6225bca04a7de73342b8b
Busiii <jennifer-li.beeler@stud.hslu.ch>
2024-06-06 10:04:23 +0200

    jj

---
+++ lvl3.js
@@ -0,0 +1,111 @@
+let currentIndex = 0;
+const images = [
+    "pictures_purple/pp1.png",
+    "pictures_purple/pp2.png",
+    "pictures_purple/pp3.png", 
+    "pictures_purple/pp4.png",
+    "pictures_purple/pp5.png",
+    "pictures_purple/pp6.png", 
+    "pictures_purple/pp7.png",
+    "pictures_purple/pp8.png",
+    "pictures_purple/pp9.png",
+];
+
+const positions = [
+    { left: '45.5%', top: '42%' },
+    { left: '54%', top: '45%' },
+    { left: '48%', top: '51%' },
+    { left: '35%', top: '61%' },
+    { left: '69.5%', top: '58.5%' },
+    { left: '47.5%', top: '49%' },
+    { left: '45%', top: '57%' },
+    { left: '49%', top: '26%' },
+    { left: '52%', top: '46%' }
+];
+
+const sizes = [
+    { width: '150px', height: '150px' },
+    { width: '65px', height: '65px' },
+    { width: '23px', height: '23px' },
+    { width: '49px', height: '49px' },
+    { width: '80px', height: '80px' },
+    { width: '57px', height: '57px' },
+    { width: '110px', height: '110px' },
+    { width: '40px', height: '40px' },
+    { width: '230px', height: '230px' }
+];
+
+console.log("init");
+
+function changeImage() {
+    console.log("changeImage");
+    console.log(currentIndex);
+    const button = document.querySelector('button');
+    const circle = document.getElementById('circle');
+
+    // Show and animate the circle
+    circle.style.display = 'block';
+    circle.classList.add('pulse');
+
+    // Add a delay before changing the image, hiding the circle, and updating button position
+    setTimeout(() => {
+        // Hide the circle
+        circle.style.display = 'none';
+        circle.classList.remove('pulse');
+
+        // Change image with fade effect
+        const image = document.getElementById('image');
+        image.classList.remove('fade-in');
+        image.classList.add('fade-out');
+
+        setTimeout(() => {
+            // Increment currentIndex before updating the image, position, and size
+            currentIndex = (currentIndex + 1) % images.length;
+
+            // Update image source
+            image.src = images[currentIndex];
+            image.classList.remove('fade-out');
+            image.classList.add('fade-in');
+
+            // Update button and circle position
+            const newPosition = positions[currentIndex];
+            button.style.left = newPosition.left;
+            button.style.top = newPosition.top;
+            circle.style.left = newPosition.left;
+            circle.style.top = newPosition.top;
+
+            // Update button and circle size
+            const newSize = sizes[currentIndex];
+            button.style.width = newSize.width;
+            button.style.height = newSize.height;
+            circle.style.width = newSize.width;
+            circle.style.height = newSize.height;
+
+            
+            if (currentIndex === 0) { 
+                window.location.href = 'levelVier.html'; 
+            }
+        }, 250); 
+
+    }, 2000); 
+}
+
+window.onload = function() {
+    document.getElementById('image').src = images[0];
+
+    const button = document.querySelector('button');
+    const initialPosition = positions[0];
+    button.style.left = initialPosition.left;
+    button.style.top = initialPosition.top;
+
+    const circle = document.getElementById('circle');
+    const initialSize = sizes[0];
+    button.style.width = initialSize.width;
+    button.style.height = initialSize.height;
+    circle.style.width = initialSize.width;
+    circle.style.height = initialSize.height;
+    circle.style.left = initialPosition.left;
+    circle.style.top = initialPosition.top;
+
+    button.addEventListener('click', changeImage);
+};
